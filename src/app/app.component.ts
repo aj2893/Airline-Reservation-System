488f3daa522d69388cde1da009d2d12a31f08a76
unknown <aniketjamkar@gmail.com>
2018-12-07 12:16:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Airline Reservation System app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import { FormsModule  } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
 import { AppComponent } from './app.component';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+const routes: Routes = [
+  { path: 'search', component: SearchFlightsComponent }
+];
+
+export class AppRoutingModule {}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFlightsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    FormsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatButtonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
